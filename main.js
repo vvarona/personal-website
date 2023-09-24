1faec4da2d915c2c7d8d58a0b03fd6cc6c20bd0f
Victoria Varona <vvaronad@gmail.com>
2023-09-25 01:13:04 +0200

    new look

--- main.js
+++
@@ -1,9 +0,0 @@
-const modal = document.getElementById("splash__container")
-
-setTimeout(() => {
-  modal.style.opacity = 0;
-  modal.classList.add("hidden")
-}, 3000)
-setTimeout(() => {
-  modal.style.display = 'none';
-}, 3100)
